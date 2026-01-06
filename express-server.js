47e9855fd188b93f8837bd156be416c1fea6f2da
TurahRae <Turahrae@gmail.com>
2026-01-06 13:16:24 +0100

    Add first Express server with basic routes

---
+++ express-server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+//Home route
+app.get("/", (req, res) => {
+    res.send("Welcome to the Express Home Page");
+});
+
+//About route
+app.get("/about", (req, res) => {
+    res.send("This is the Express About Page");
+});
+
+//API route
+app.get("/api", (req, res) => {
+    res.json({message: "Hello from Express API"});
+});
+
+//404 route
+app.use((req, res) => {
+    res.status(404).send("page not found");
+});
+
+//Start server
+app.listen(PORT, () => {
+    console.log(`Express server running on http://localhost:${PORT}`);
+});
