5174f3777e7169aad8382a9c922137673536d322
TurahRae <Turahrae@gmail.com>
2026-01-07 16:42:34 +0100

    Add os-example to demostrate os module

--- os-example.js
+++ os-example.js
@@ -1,5 +1,11 @@
 const os = require("os");
 
-console.log("Platform:", os.platform());
-console.log("CPU cores:", os.cpus().length);
-console.log("Free memory (MB):", os.freemem() / 1024 / 1024);
+// Basic info
+console.log("Operating System Info:");
+console.log("OS Type:", os.type());
+console.log("OS Platform:", os.platform());
+console.log("Architecture:", os.arch());
+console.log("CPU Cores:", os.cpus().length);
+console.log("Total Memory:", (os.totalmem() / (1024 ** 3)).toFixed(2), "GB");
+console.log("Free Memory:", (os.freemem() / (1024 ** 3)).toFixed(2), "GB");
+console.log("Uptime (seconds):", os.uptime());
