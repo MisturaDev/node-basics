66dd6b3dbad0ff034237e40d7a8805c316478ab9
TurahRae <Turahrae@gmail.com>
2026-01-11 18:58:10 +0100

    Use DEFAULT_ROLE from .env in Profile API when role is missing

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,18 +1,22 @@
 const createProfile = (req, res) => {
- const { name, role } = req.body;
+  const { name, role } = req.body;
 
-    //Validation
-    if (!name || !role) {
-        return res.status(400).json({ message: "Name and role are required"});
-        }
-    
-        //if data is valid
-        res.status(201).json({
-        message: "Profile created successfully",
-        data: { name, role },
-    });
+// Use default role from .env if role is missing
+  const finalRole = role || process.env.DEFAULT_ROLE;
+
+  // Validation: name is required
+  if (!name) {
+    return res.status(400).json({ message: "Name is required" });
+  }
+
+  // Respond with profile data
+  res.status(201).json({
+    message: "Profile created successfully",
+    data: {
+      name,
+      role: finalRole,
+    },
+  });
 };
 
-module.exports = { 
-    createProfile,
- };
+module.exports = { createProfile };
